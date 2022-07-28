485faafc2a1eced2dbe3b76029b35694f1683c47
GenaBercini <bercinigena@gmail.com>
2022-07-28 19:01:30 -0300

    Changes for deploy front

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import store from "../src/redux/store/store"
 import axios from 'axios';
 import dotenv from "dotenv";
 import 'dotenv/config'
+
 dotenv.config();
 axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
@@ -16,7 +17,3 @@ root.render(
   <App />
 </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
